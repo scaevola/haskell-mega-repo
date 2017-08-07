a6a760ac8919ae769e5736c4643f91af9b5b13da
ArktinenSieni <matti.raty@futurice.com>
2017-08-07 13:49:01 +0300

    rebase, reordering ContractType

--- personio-client/src/Personio/Types/ContractType.hs
+++ personio-client/src/Personio/Types/ContractType.hs
@@ -13,6 +13,7 @@ module Personio.Types.ContractType (
 import Futurice.Generics
 import Futurice.Generics.Enum
 import Futurice.Prelude
+import Lucid                  (ToHtml (..))
 
 data ContractType
     = PermanentAllIn
@@ -23,18 +24,6 @@ data ContractType
 makePrisms ''ContractType
 deriveGeneric ''ContractType
 deriveLift ''ContractType
-
-instance NFData ContractType
-
-instance Arbitrary ContractType where
-    arbitrary = sopArbitrary
-    shrink    = sopShrink
-
-instance ToJSON ContractType where
-    toJSON = enumToJSON ei
-
-instance FromJSON ContractType where
-    parseJSON = enumParseJSON ei 
 
 ei :: EnumInstances ContractType
 ei = sopEnumInstances $ 
@@ -47,11 +36,39 @@ ei = sopEnumInstances $ 
 -- Boilerplate
 -------------------------------------------------------------------------------
 
-_ContractType :: Prism' Text ContractType
-_ContractType = enumPrism ei
+contractTypeToText :: ContractType -> Text
+contractTypeToText = enumToText ei
 
 contractTypeFromText :: Text -> Maybe ContractType
 contractTypeFromText = enumFromText ei
 
-contractTypeToText :: ContractType -> Text
-contractTypeToText = enumToText ei
+_ContractType :: Prism' Text ContractType
+_ContractType = enumPrism ei
+
+instance NFData ContractType
+
+instance Arbitrary ContractType where
+    arbitrary = sopArbitrary
+    shrink    = sopShrink
+
+instance ToHtml ContractType where
+    toHtmlRaw = toHtml
+    toHtml = toHtml . enumToText ei
+
+instance ToParamSchema ContractType where
+    toParamSchema = enumToParamSchema ei
+
+instance ToSchema ContractType where
+    declareNamedSchema = enumDeclareNamedSchema ei
+
+instance ToJSON ContractType where
+    toJSON = enumToJSON ei
+
+instance FromJSON ContractType where
+    parseJSON = enumParseJSON ei
+
+instance FromHttpApiData ContractType where
+    parseUrlPiece = enumParseUrlPiece ei
+
+instance ToHttpApiData ContractType where
+    toUrlPiece = enumToUrlPiece ei
