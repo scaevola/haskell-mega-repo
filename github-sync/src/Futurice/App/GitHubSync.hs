2fdf256f321b8a38f80bd6f76b78a20e33ec4c17
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-09-07 10:57:42 +0300

    WIP: github-sync

---
+++ github-sync/src/Futurice/App/GitHubSync.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+module Futurice.App.GitHubSync (defaultMain) where
+
+import Futurice.Integrations
+import Futurice.Lucid.Foundation (HtmlPage)
+import Futurice.Prelude
+import Futurice.Servant
+import Prelude ()
+import Servant
+
+import Futurice.App.GitHubSync.API
+import Futurice.App.GitHubSync.Config
+import Futurice.App.GitHubSync.Ctx
+import Futurice.App.GitHubSync.IndexPage
+
+import qualified FUM.Types.Login as FUM
+import qualified GitHub          as GH
+import qualified Personio        as P
+
+server :: Ctx -> Server GitHubSyncAPI
+server ctx = indexPageAction ctx
+    -- TODO: actions to add & remove
+
+indexPageAction
+    :: Ctx
+    -> Maybe FUM.Login
+    -> Handler (HtmlPage "index")
+indexPageAction ctx _mfu = do
+    -- TODO: restrict access to IT only
+    now <- currentTime
+    (gh, p) <- liftIO $
+        runIntegrations mgr lgr now (cfgIntegrationsConfig cfg) fetcher
+    pure $ indexPage now (cfgPinnedUsers cfg) gh p
+  where
+    cfg = ctxConfig ctx
+    lgr = ctxLogger ctx
+    mgr = ctxManager ctx
+
+type M = Integrations Proxy Proxy I Proxy I
+
+fetcher :: M ([GH.User], [P.Employee])
+fetcher = liftA2 (,) github personioE
+  where
+    github = toList <$> githubDetailedMembers
+    personioE = P.personio P.PersonioEmployees
+
+githubDetailedMembers :: M (Vector GH.User)
+githubDetailedMembers = do
+    githubMembers <- githubOrganisationMembers
+    traverse (githubReq . GH.userInfoForR . GH.simpleUserLogin) githubMembers
+
+defaultMain :: IO ()
+defaultMain = futuriceServerMain makeCtx $ emptyServerConfig
+    & serverName              .~ "GitHub Sync"
+    & serverDescription       .~ "Sync people from personio to github"
+    & serverApp githubSyncApi .~ server
+    & serverColour            .~  (Proxy :: Proxy ('FutuAccent 'AF3 'AC1))
+    & serverEnvPfx            .~ "GITHUBSYNC"
+  where
+    makeCtx :: Config -> Logger -> DynMapCache -> IO (Ctx, [Job])
+    makeCtx cfg lgr _cache = do
+        mgr <- newManager tlsManagerSettings
+        let ctx = Ctx cfg lgr mgr
+        pure (ctx, [])
---
+++ github-sync/src/Futurice/App/GitHubSync/Config.hs
@@ -0,0 +1,37 @@
+module Futurice.App.GitHubSync.Config (
+    Config (..),
+    Pinned (..),
+    ) where
+
+import Futurice.EnvConfig
+import Futurice.Integrations
+import Futurice.Prelude
+import Prelude ()
+import Text.Regex.Applicative (RE, match, psym, sym)
+
+import qualified GitHub as GH
+
+data Config = Config
+    { cfgIntegrationsConfig :: !(IntegrationsConfig Proxy Proxy I Proxy I) -- TODO
+    , cfgAuth               :: !GH.Auth
+    , cfgOrganisationName   :: !(GH.Name GH.Organization)
+    , cfgPinnedUsers        :: !Pinned
+    }
+
+instance Configure Config where
+    configure = Config
+        <$> configure
+        <*> envVar "GH_AUTH_TOKEN"
+        <*> envVar "GH_ORG"
+        <*> envVar "GITHUBSYNC_PINNEDGHUSERS"
+
+newtype Pinned = Pin { unPin :: [GH.Name GH.User] }
+
+instance FromEnvVar Pinned where
+    fromEnvVar = match pinnedRegex
+
+pinnedRegex :: RE Char Pinned
+pinnedRegex = fmap Pin $ pure []
+    <|> liftA2 (:) single (many $ sym ',' *> single)
+  where
+    single = GH.mkUserName . view packed <$> some (psym (`notElem` ", "))
