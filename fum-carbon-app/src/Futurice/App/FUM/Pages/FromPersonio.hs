a1c48dcc745928aac441c173a1be09a7305d4455
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-09-12 20:36:12 +0300

    Filter created users in FromPersonio

--- fum-carbon-app/src/Futurice/App/FUM/Pages/FromPersonio.hs
+++ fum-carbon-app/src/Futurice/App/FUM/Pages/FromPersonio.hs
@@ -4,12 +4,12 @@
 {-# LANGUAGE RecordWildCards   #-}
 module Futurice.App.FUM.Pages.FromPersonio (fromPersonioPage) where
 
-import Prelude ()
-import Futurice.Prelude
-import Futurice.IdMap (IdMap)
-
+import Data.Maybe              (isNothing)
 import Futurice.App.FUM.Markup
 import Futurice.App.FUM.Types
+import Futurice.IdMap          (IdMap)
+import Futurice.Prelude
+import Prelude ()
 
 import qualified Personio
 
@@ -18,7 +18,7 @@ fromPersonioPage
     -> World                    -- ^ the world
     -> IdMap Personio.Employee  -- ^ employees
     -> HtmlPage "from-personio"
-fromPersonioPage auth _world es = fumPage_ "FUM" auth $ do
+fromPersonioPage auth world es = fumPage_ "FUM" auth $ do
     fumHeader_  "Personio users" []
 
     fullRow_ $ table_ $ do
@@ -34,15 +34,16 @@ fromPersonioPage auth _world es = fumPage_ "FUM" auth $ do
             th_ "end date"
             th_ "status"
             th_ "create"
-        tbody_ $ for_ es $ \Personio.Employee {..} -> tr_ $ do
-            td_ $ toHtml _employeeId
-            td_ $ toHtml _employeeFirst
-            td_ $ toHtml _employeeLast
-            td_ $ traverse_ toHtml _employeeLogin
-            td_ $ toHtml _employeeTribe
-            td_ $ toHtml _employeeOffice
-            td_ $ traverse_ toHtml _employeeCostCenter
-            td_ $ traverse_ (toHtml . show) _employeeHireDate
-            td_ $ traverse_ (toHtml . show) _employeeEndDate
-            td_ $ toHtml _employeeStatus
-            td_ $ futuLinkButton_ (createEmployeeHref_ _employeeId) "Create"
+        tbody_ $ for_ es $ \Personio.Employee {..} -> tr_ $
+            when (isNothing $ _employeeLogin >>= \login -> world ^? worldEmployees . ix login) $ do
+                td_ $ toHtml _employeeId
+                td_ $ toHtml _employeeFirst
+                td_ $ toHtml _employeeLast
+                td_ $ traverse_ toHtml _employeeLogin
+                td_ $ toHtml _employeeTribe
+                td_ $ toHtml _employeeOffice
+                td_ $ traverse_ toHtml _employeeCostCenter
+                td_ $ traverse_ (toHtml . show) _employeeHireDate
+                td_ $ traverse_ (toHtml . show) _employeeEndDate
+                td_ $ toHtml _employeeStatus
+                td_ $ futuLinkButton_ (createEmployeeHref_ _employeeId) "Create"
