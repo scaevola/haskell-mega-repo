6f31bbbc9da5ee4db95f2fc80efd6dde25cc3947
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-10-29 13:00:52 +0200

    Add more doctests, similarity props

--- kleene/src/Kleene/Sets.hs
+++ kleene/src/Kleene/Sets.hs
@@ -1,6 +1,7 @@
 -- | Character sets.
 module Kleene.Sets (
     dotRSet,
+    rsetToJS,
     ) where
 
 import Data.RangeSet.List  (RSet)
@@ -9,3 +10,14 @@ import qualified Data.RangeSet.List as RSet
 
 dotRSet :: RSet Char
 dotRSet = RSet.full RSet.\\ RSet.singleton '\n'
+
+-- TODO: escaping, ranges
+rsetToJS :: RSet Char -> ShowS
+rsetToJS cs
+    = showChar '['
+    . foldMap f (RSet.toRangeList cs)
+    . showChar ']'
+  where
+    f (a, b)
+      | a == b = showChar a
+      | otherwise = showChar a . showChar '-' . showChar b
